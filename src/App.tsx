86db75223e4a5bc3bcfef2dd58e5176c596a7338
Volodymyr Sen <volodymyr.sen@gmail.com>
2022-11-08 17:10:52 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Learn React</div>
+    </div>
+  );
+}
+
+export default App;
